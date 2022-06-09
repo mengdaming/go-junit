2f4676a21a97c98b57bdae3732af8673fd4a8938
Damien Menanteau <damien.menanteau@gmail.com>
2022-06-09 18:59:55 +0200

    Put back tests that were broken due to CRLF

--- ingest_test.go
+++ ingest_test.go
@@ -69,16 +69,16 @@ func TestExamplesInTheWild(t *testing.T) {
 				assert.Equal(t, "file_test.go:11: Skip message", suites[0].Tests[0].Message)
 			},
 		},
-		//{
-		//	title:    "ibm example",
-		//	filename: "testdata/ibm.xml",
-		//	origin:   "https://www.ibm.com/support/knowledgecenter/en/SSQ2R2_14.2.0/com.ibm.rsar.analysis.codereview.cobol.doc/topics/cac_useresults_junit.html",
-		//	check: func(t *testing.T, suites []Suite) {
-		//		assert.Len(t, suites, 1)
-		//		assert.Len(t, suites[0].Tests, 1)
-		//		assert.EqualError(t, suites[0].Tests[0].Error, "\nWARNING: Use a program name that matches the source file name\nCategory: COBOL Code Review – Naming Conventions\nFile: /project/PROGRAM.cbl\nLine: 2\n      ")
-		//	},
-		//},
+		{
+			title:    "ibm example",
+			filename: "testdata/ibm.xml",
+			origin:   "https://www.ibm.com/support/knowledgecenter/en/SSQ2R2_14.2.0/com.ibm.rsar.analysis.codereview.cobol.doc/topics/cac_useresults_junit.html",
+			check: func(t *testing.T, suites []Suite) {
+				assert.Len(t, suites, 1)
+				assert.Len(t, suites[0].Tests, 1)
+				assert.EqualError(t, suites[0].Tests[0].Error, "\nWARNING: Use a program name that matches the source file name\nCategory: COBOL Code Review – Naming Conventions\nFile: /project/PROGRAM.cbl\nLine: 2\n      ")
+			},
+		},
 		{
 			title:    "jenkinsci example",
 			filename: "testdata/jenkinsci.xml",
@@ -90,88 +90,88 @@ func TestExamplesInTheWild(t *testing.T) {
 				assert.Equal(t, `\`, suites[0].Properties["file.separator"])
 			},
 		},
-		//{
-		//	title:    "nose2 example",
-		//	filename: "testdata/nose2.xml",
-		//	origin:   "https://nose2.readthedocs.io/en/latest/plugins/junitxml.html",
-		//	check: func(t *testing.T, suites []Suite) {
-		//		assert.Len(t, suites, 1)
-		//		assert.Len(t, suites[0].Tests, 25)
-		//		assert.EqualError(t, suites[0].Tests[22].Error, "Traceback (most recent call last):\n  File \"nose2/tests/functional/support/scenario/tests_in_package/pkg1/test/test_things.py\", line 13, in test_typeerr\n    raise TypeError(\"oops\")\nTypeError: oops\n")
-		//	},
-		//},
-		//{
-		//	title:    "python junit-xml example",
-		//	filename: "testdata/python-junit-xml.xml",
-		//	origin:   "https://pypi.org/project/junit-xml/",
-		//	check: func(t *testing.T, suites []Suite) {
-		//		assert.Len(t, suites, 1)
-		//		assert.Len(t, suites[0].Tests, 1)
-		//		assert.Equal(t, "\n                I am stdout!\n            ", suites[0].Tests[0].SystemOut)
-		//		assert.Equal(t, "\n                I am stderr!\n            ", suites[0].Tests[0].SystemErr)
-		//	},
-		//},
-		//{
-		//	title:    "surefire example",
-		//	filename: "testdata/surefire.xml",
-		//	origin:   "https://gist.github.com/rwbergstrom/6f0193b1a12dca9d358e6043ee6abba4",
-		//	check: func(t *testing.T, suites []Suite) {
-		//		assert.Len(t, suites, 1)
-		//		assert.Len(t, suites[0].Tests, 1)
-		//		assert.Equal(t, "\n", suites[0].Properties["line.separator"])
-		//		assert.Equal(t, "Hello, World\n", suites[0].Tests[0].SystemOut)
-		//		assert.Equal(t, "I'm an error!\n", suites[0].Tests[0].SystemErr)
-		//
-		//		var testcase = Test{
-		//			Name:      "testStdoutStderr",
-		//			Classname: "com.example.FooTest",
-		//			Duration:  1234560 * time.Millisecond,
-		//			Status:    StatusFailed,
-		//			Error: Error{
-		//				Type: "java.lang.AssertionError",
-		//				Body: "java.lang.AssertionError\n\tat com.example.FooTest.testStdoutStderr(FooTest.java:13)\n",
-		//			},
-		//			Properties: map[string]string{
-		//				"classname": "com.example.FooTest",
-		//				"name":      "testStdoutStderr",
-		//				"time":      "1,234.56",
-		//			},
-		//			SystemOut: "Hello, World\n",
-		//			SystemErr: "I'm an error!\n",
-		//		}
-		//
-		//		assert.Equal(t, testcase, suites[0].Tests[0])
-		//	},
-		//},
-		//{
-		//	title:    "fastlane example",
-		//	filename: "testdata/fastlane-trainer.xml",
-		//	check: func(t *testing.T, suites []Suite) {
-		//		assert.Len(t, suites, 1)
-		//		assert.Len(t, suites[0].Tests, 4)
-		//
-		//		var testcase = Test{
-		//			Name:      "testSomething()",
-		//			Classname: "TestClassSample",
-		//			Duration:  342 * time.Millisecond,
-		//			Status:    StatusFailed,
-		//			Message:   "XCTAssertTrue failed",
-		//			Error: Error{
-		//				Message: "XCTAssertTrue failed",
-		//				Body:    "\n            ",
-		//			},
-		//			Properties: map[string]string{
-		//				"classname": "TestClassSample",
-		//				"name":      "testSomething()",
-		//				"time":      "0.342",
-		//			},
-		//		}
-		//
-		//		assert.Equal(t, testcase, suites[0].Tests[2])
-		//		assert.EqualError(t, suites[0].Tests[2].Error, "XCTAssertTrue failed")
-		//		assert.EqualError(t, suites[0].Tests[3].Error, "NullPointerException")
-		//	},
-		//},
+		{
+			title:    "nose2 example",
+			filename: "testdata/nose2.xml",
+			origin:   "https://nose2.readthedocs.io/en/latest/plugins/junitxml.html",
+			check: func(t *testing.T, suites []Suite) {
+				assert.Len(t, suites, 1)
+				assert.Len(t, suites[0].Tests, 25)
+				assert.EqualError(t, suites[0].Tests[22].Error, "Traceback (most recent call last):\n  File \"nose2/tests/functional/support/scenario/tests_in_package/pkg1/test/test_things.py\", line 13, in test_typeerr\n    raise TypeError(\"oops\")\nTypeError: oops\n")
+			},
+		},
+		{
+			title:    "python junit-xml example",
+			filename: "testdata/python-junit-xml.xml",
+			origin:   "https://pypi.org/project/junit-xml/",
+			check: func(t *testing.T, suites []Suite) {
+				assert.Len(t, suites, 1)
+				assert.Len(t, suites[0].Tests, 1)
+				assert.Equal(t, "\n                I am stdout!\n            ", suites[0].Tests[0].SystemOut)
+				assert.Equal(t, "\n                I am stderr!\n            ", suites[0].Tests[0].SystemErr)
+			},
+		},
+		{
+			title:    "surefire example",
+			filename: "testdata/surefire.xml",
+			origin:   "https://gist.github.com/rwbergstrom/6f0193b1a12dca9d358e6043ee6abba4",
+			check: func(t *testing.T, suites []Suite) {
+				assert.Len(t, suites, 1)
+				assert.Len(t, suites[0].Tests, 1)
+				assert.Equal(t, "\n", suites[0].Properties["line.separator"])
+				assert.Equal(t, "Hello, World\n", suites[0].Tests[0].SystemOut)
+				assert.Equal(t, "I'm an error!\n", suites[0].Tests[0].SystemErr)
+
+				var testcase = Test{
+					Name:      "testStdoutStderr",
+					Classname: "com.example.FooTest",
+					Duration:  1234560 * time.Millisecond,
+					Status:    StatusFailed,
+					Error: Error{
+						Type: "java.lang.AssertionError",
+						Body: "java.lang.AssertionError\n\tat com.example.FooTest.testStdoutStderr(FooTest.java:13)\n",
+					},
+					Properties: map[string]string{
+						"classname": "com.example.FooTest",
+						"name":      "testStdoutStderr",
+						"time":      "1,234.56",
+					},
+					SystemOut: "Hello, World\n",
+					SystemErr: "I'm an error!\n",
+				}
+
+				assert.Equal(t, testcase, suites[0].Tests[0])
+			},
+		},
+		{
+			title:    "fastlane example",
+			filename: "testdata/fastlane-trainer.xml",
+			check: func(t *testing.T, suites []Suite) {
+				assert.Len(t, suites, 1)
+				assert.Len(t, suites[0].Tests, 4)
+
+				var testcase = Test{
+					Name:      "testSomething()",
+					Classname: "TestClassSample",
+					Duration:  342 * time.Millisecond,
+					Status:    StatusFailed,
+					Message:   "XCTAssertTrue failed",
+					Error: Error{
+						Message: "XCTAssertTrue failed",
+						Body:    "\n            ",
+					},
+					Properties: map[string]string{
+						"classname": "TestClassSample",
+						"name":      "testSomething()",
+						"time":      "0.342",
+					},
+				}
+
+				assert.Equal(t, testcase, suites[0].Tests[2])
+				assert.EqualError(t, suites[0].Tests[2].Error, "XCTAssertTrue failed")
+				assert.EqualError(t, suites[0].Tests[3].Error, "NullPointerException")
+			},
+		},
 		{
 			title:    "phpunit example",
 			filename: "testdata/phpunit.xml",
